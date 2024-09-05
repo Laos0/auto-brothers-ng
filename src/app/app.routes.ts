b827f3f21a936159a9f45ac3367c4272e5002c15
laos0 <isonylao@gmail.com>
2024-09-05 13:09:04 -0500

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
-import { Routes } from '@angular/router';
+import { provideRouter, Routes } from '@angular/router';
+import { HomeComponent } from './views/home/home.component';
+import { AboutUsComponent } from './views/about-us/about-us.component';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'home', component: HomeComponent },
+    { path: 'about-us', component: AboutUsComponent }
+];
+
+// this is the standalone router setup using bootstrapApplication call
+bootstrapApplication(AppComponent, {
+    providers: [provideRouter(routes)]
+});
