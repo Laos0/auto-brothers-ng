1ad07093dc2031a4bca5ca3a5884c9cb974a288d
laos0 <isonylao@gmail.com>
2024-09-07 07:43:54 -0500

    added in a nav bar along with routes to components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,16 @@ import { HomeComponent } from './views/home/home.component';
 import { AboutUsComponent } from './views/about-us/about-us.component';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { OurServiceComponent } from './views/our-service/our-service.component';
+import { ContactUsComponent } from './views/contact-us/contact-us.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'home', component: HomeComponent },
-    { path: 'about-us', component: AboutUsComponent }
+    { path: 'about-us', component: AboutUsComponent },
+    { path: 'service', component: OurServiceComponent},
+    { path: 'contact', component: ContactUsComponent}
+    
 ];
 
 // this is the standalone router setup using bootstrapApplication call
--- src/app/views/header/header.component.ts
+++ src/app/views/header/header.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { NavBarComponent } from "../../navigations/nav-bar/nav-bar.component";
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterModule],
+  imports: [NavBarComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
